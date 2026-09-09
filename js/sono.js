b803e21977ffe51a1d6d4ad82788374d68ef236e
peakbe <xpb66bfnpd@privaterelay.appleid.com>
2026-09-09 14:13:31 +0200

    Rename updateNdSonometers to renderSonometersALLDynamic

--- js/sono.js
+++ js/sono.js
@@ -135,7 +135,7 @@ createMarkers(sonometersEBLG);
 // ===============================================================
 // MODE ALL DYNAMIQUE — ND Airbus PRO+++
 // ===============================================================
-export async function updateNdSonometers() {
+export async function renderSonometersALLDynamic() {
 
   // 1) Lire METAR EBLG + EBCI via ton Worker
   const metarEBLG = await fetch(`${WORKER_BASE_URL}/api/metar?station=EBLG`)
