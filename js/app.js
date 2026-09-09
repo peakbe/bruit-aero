63345ee8ef9ad5746a7bfbc5834a58e0f8b9aaf1
peakbe <xpb66bfnpd@privaterelay.appleid.com>
2026-09-09 15:08:03 +0200

    Fix import statement for renderSonometers function

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 // ===============================================================
 import { map, initRadarMap, drawApproachDepartureCones } from "./map.js";
 import { updateFIDS } from "./fids.js";
-import { renderSonometers, renderSonometersALLDynami, sonoLayer } from "./sono.js";
+import { renderSonometers, renderSonometersALLDynamic, sonoLayer } from "./sono.js";
 import {
   updateWindTrend,
   updateNdWindComponents,
